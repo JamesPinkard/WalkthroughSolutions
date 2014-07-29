cb848d5a9667458bbc2ccbb7900c8181993d55ce
JamesPinkard <James.b.Pinkard@gmail.com>
2014-07-29 10:26:44 -0500

    Picture View and WPF excercises
    I finished the WPF MyWordPad Example, and started the WPF API and
    Controls example

---
+++ MyWordPad/MyWordPad/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace MyWordPad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetF1CommandBinding();
+        }
+
+        private void SetF1CommandBinding()
+        {
+            CommandBinding helpBinding = new CommandBinding(ApplicationCommands.Help);
+            helpBinding.CanExecute += CanHelpExecute;
+            helpBinding.Executed += HelpExecuted;
+            CommandBindings.Add(helpBinding);
+        }
+
+        private void CanHelpExecute(object sender, CanExecuteRoutedEventArgs e)
+        {            
+            // Here you can set CanExecute to false if you want to prevent the command from executing
+            e.CanExecute = true;
+        }
+
+        private void HelpExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Look, it is not that difficult. Just type something!", "Help!");
+        }
+        
+        protected void FileExit_Click(object sender, RoutedEventArgs e)
+        {
+            // Close this window.
+            this.Close();
+        }
+
+        protected void ToolsSpellingHints_Click(object sender, RoutedEventArgs e)
+        {
+            string spellingHints = string.Empty;
+
+            SpellingError error = txtData.GetSpellingError(txtData.CaretIndex);
+            if (error != null)
+            {
+                // Build a string of spelling suggestions
+                foreach (string s in error.Suggestions)
+                {
+                    spellingHints += string.Format("{0}\n", s);
+                }
+
+                // Show suggestions and expand the expander.
+                lblSpellingHints.Content = spellingHints;
+                expanderSpelling.IsExpanded = true;
+            }
+        }
+
+        private void MouseEnterExitArea(object sender, MouseEventArgs e)
+        {
+            statBarText.Text = "Exit the Application";
+        }
+
+        protected void MouseLeaveArea(object sender, MouseEventArgs e)
+        {
+            statBarText.Text = "Ready";
+        }
+
+        protected void MouseEnterToolsHintsArea(object sender, MouseEventArgs e)
+        {
+            statBarText.Text = "Show Spelling Suggestions";
+        }
+
+        private void OpenCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OpenCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            // Create an open file dialog box and only show txt or csv files.
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.Filter = "Text Files |*.txt| Comma Seperated Values |*.csv";
+
+            // Did they click on the OK button?
+            if (true == openDlg.ShowDialog())
+            {
+                // Load all text of selected file.
+                string dataFromFile = File.ReadAllText(openDlg.FileName);
+
+                // Show string in TextBox.
+                txtData.Text = dataFromFile;
+            }
+        }
+
+        private void SaveCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void SaveCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Filter = "Text Files | *.txt";
+
+            // Did they click on the OK button?
+            if (true == saveDlg.ShowDialog())
+            {
+                // Save data in the TextBox to the named file.
+                File.WriteAllText(saveDlg.FileName, txtData.Text);
+            }
+        }
+    }
+}
