79ef4a3610aaed0f088bf723c1e53e1872040d2f
JamesPinkard <James.b.Pinkard@gmail.com>
2014-08-05 14:18:00 -0500

    Worked on color changing paint window.

--- WpfControlsAndAPIs/WpfControlsAndAPIs/MainWindow.xaml.cs
+++ WpfControlsAndAPIs/WpfControlsAndAPIs/MainWindow.xaml.cs
@@ -23,6 +23,41 @@ namespace WpfControlsAndAPIs
         public MainWindow()
         {
             InitializeComponent();
+            
+            // Be in Ink mode by defualt.
+            this.myInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            this.inkRadio.IsChecked = true;
+            this.comboColors.SelectedIndex = 0;            
+        }
+
+        private void RadioButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            /* Based on which button sent the event,
+             * place the InkCanvas in a unique mode of operation */
+            switch ((sender as RadioButton).Content.ToString())
+            {
+                /* These strings must be the same
+                 * as the Content values for each RadioButton */
+                case "Ink Mode!":
+                    this.myInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                    break;
+                case "Erase Mode!":
+                    this.myInkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+                    break;
+                case "Select Mode!":
+                    this.myInkCanvas.EditingMode = InkCanvasEditingMode.Select;
+                    break;
+            }
+             
+        }
+
+        private void Color_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            // Get the Tag of the selected StackPanel.
+            string colorToUse = (this.comboColors.SelectedItem as StackPanel).Tag.ToString();
+
+            // Change the color used to render the strokes.
+            this.myInkCanvas.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString(colorToUse);
         }
     }
 }
