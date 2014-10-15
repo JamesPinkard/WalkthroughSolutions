3fdcf81c2acc261452e515b5261a668570bff23d
JamesPinkard <James.b.Pinkard@gmail.com>
2014-10-15 10:50:15 -0500

    Started going through Pro C# examples Chapter 3

---
+++ Pro_C#_AndTheNetFramework/Chapter_3_CoreConstructs/CoreConstructs_pt1/CoreConstructs_pt1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreConstructs_pt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Console I/O *****");
+            DisplayMessage();
+            Console.ReadLine();
+        }
+        
+        static void GetUserData()
+        {
+            // Get name and age.
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            string userAge = Console.ReadLine();
+
+            // Change echo color, just for fun.
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            // Echo to the console.
+            Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
+
+            // Restore previous color.
+            Console.ForegroundColor = prevColor;
+        }
+
+        static void FormatNumericalData()
+        {
+            // format tags
+            Console.WriteLine("The value 99999 in various formats:");
+            Console.WriteLine("c format: {0:c}", 99999);
+            Console.WriteLine("d9 format: {0:d9}", 99999);
+            Console.WriteLine("f3 format: {0:f3}", 99999);
+            Console.WriteLine("n format: {0:n}", 99999);
+
+            // Notice that upper- or lowercasing for hex
+            // determines if letters are upper- or lowercase.
+            Console.WriteLine("E format: {0:E}", 99999);
+            Console.WriteLine("e format: {0:e}", 99999);
+            Console.WriteLine("X format: {0:X}", 99999);
+            Console.WriteLine("x format: {0:x}", 99999);
+        }
+        
+        static void DisplayMessage()
+        {
+            // Using string.Format() to format a string literal.
+            string userMessage = string.Format("100000 in hex is {0:x}", 100000);
+
+            // You need to reference PresentationFramework.dll
+            // in order to compile this line of code!
+            System.Windows.MessageBox.Show(userMessage);
+        }        
+    }
+}
