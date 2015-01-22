2b68af6088067828d2bf3c0fd408075f8ad248f4
JamesPinkard <James.b.Pinkard@gmail.com>
2015-01-22 16:12:16 -0600

    Almost Done with Chapter 7

---
+++ Pro_C#_AndTheNetFramework/Chapter_8_Interfaces/CustomEnumeratorWYield/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEnumeratorWYield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
