3f64960917544c52b03d8243d384105688c5a7ad
JamesPinkard <James.b.Pinkard@gmail.com>
2015-02-19 08:42:44 -0600

    On Timer Callbacks in Chapter 19

---
+++ Pro_C#_AndTheNetFramework/Chapter_19_Parallel/MultiThreadedPrinting/Printer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultiThreadedPrinting
+{
+    class Printer
+    {
+        // Lock token.
+        private object threadLock =new object();
+
+        public void PrintNumbers()
+        {
+            lock(threadLock)
+            {
+                // Display Thread info.
+                Console.WriteLine("-> {0} is executing PrintNumbers()", Thread.CurrentThread.Name);
+
+                // Print out numbers.
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    // Put thread to sleep for a random amount of time.
+                    Random r = new Random();
+                    Thread.Sleep(1000 * r.Next(5));
+                    Console.Write("{0}, ", i);                
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
